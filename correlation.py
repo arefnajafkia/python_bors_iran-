75549abe13ceeea277ed2fecf3a21c8082c514b1
arefnajafkia <arefnajafkia294@gmail.com>
2023-12-26 16:32:44 -0800

    add new correlation complet

--- correlation.py
+++ correlation.py
@@ -13,7 +13,11 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv ("Foolad.csv",index_col=0,parse_dates=True)
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as pyplot
+%matplotlib notebook
+
+#df = pd.read_csv ("Foolad.csv",index_col=0,parse_dates=True)
 tickers = ['Nafti','Khodro','Foolad','Felezi','Daroo']
 
 df_list = []
@@ -24,4 +28,42 @@ for ticker in tickers:
 data = pd.concat(df_list,axis=1)
 data.columns = tickers
 data = np.log(data/data.shift())
-data.corr().style.background_gradient(cmap='Blues')
+#data.corr().style.background_gradient(cmap='Blues')
+
+def lin_regr (ticker_a,ticker_b):
+    X = data[ticker_a].iloc[1:].values.reshape(-1, 1)
+    Y = data[ticker_b].iloc[1:].values.reshape(-1, 1)
+
+    lin_regressor = LinearRegression()
+    lin_regressor.fit(X, Y)
+    Y_pred = lin_regressor.predict(X)
+
+    alpha = str(round(lin_regressor.intercept_[0], 5))
+    beta = str(round(lin_regressor.coef_[0][0], 5))
+
+    fig, ax = plt.subplots()
+    ax.set_title("Alpha: " + alpha + ",Beta: " + beta)
+    ax.scatter(X, Y, alpha=0.3)
+    ax.plot(X, Y_pred, c='r')
+
+
+def calc_beta(ticker_a, ticker_b)
+    X = data[ticker_a].iloc[1:].values.reshape(-1, 1)
+    Y = data[ticker_b].iloc[1:].values.reshape(-1, 1)
+
+    lin_regressor = LinearRegression()
+    lin_regressor.fit(X, Y)
+    Y_pred = lin_regressor.predict(X)
+
+    return lin_regressor.coef_[0][0]
+
+import itertools
+
+df = pd.DataFrame(None, index=tickers, columns=tickers)
+for t1, t2 in itertools.combinations(tickers, 2):
+    df.loc[t1][t2] = calc_beta(t1, t2)
+    df.loc[t2][t1] = calc_beta(t2, t1)
+
+
+for t in tickers:
+    df .loc[t][t] = calc_beta(t, t)
