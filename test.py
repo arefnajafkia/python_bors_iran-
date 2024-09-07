793eebbbd0fdd3affcb5d8bcb69b2a6b64390b5d
arefnajafkia <arefnajafkia294@gmail.com>
2024-09-07 16:28:37 +0330

    add new car in for ticker now

--- test.py
+++ test.py
@@ -30,11 +30,10 @@ while True:
           open_test_py()
           
           try:
+            
                 import math
                 import numpy as np
                 import pandas as pd
-              
-
                 import math
                 import numpy as np
                 import pandas as pd
